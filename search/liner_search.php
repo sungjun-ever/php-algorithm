d0b942dd74c596c977cf8926547ba59191529e97
sungjun-ever <zkfmak10@gmail.com>
2021-10-14 15:33:56 +0900

    'liner'

--- search/liner_search.php
+++ search/liner_search.php
@@ -1,4 +1,5 @@
 <?php
+/*
 function liner_search(array $a, $key)
 {
     for($i=0; $i<count($a); $i++){
@@ -11,5 +12,24 @@ function liner_search(array $a, $key)
 
 $arr = array(1, 2, 3, 4, 5);
 $r = liner_search($arr, 4);
+var_dump($r);
+*/
+
+function liner_search(array $a, $key)
+{
+    $i = 0;
+    while(true){
+        if($i == count($a)){
+            return false;
+        }
+        if($a[$i] == $key){
+            return $i;
+        }
+        $i++;
+    }
+}
+
+$arr = [1, 2 ,3 ,4 ,5];
+$r = liner_search($arr, 6);
 var_dump($r);
 
