fe6005eae78fa95558c62a85f7095ceda54935ac
sungjun-ever <zkfmak10@gmail.com>
2021-10-14 15:52:49 +0900

    'liner'

--- search/liner_search.php
+++ search/liner_search.php
@@ -15,6 +15,7 @@ $r = liner_search($arr, 4);
 var_dump($r);
 */
 
+/*
 function liner_search(array $a, $key)
 {
     $i = 0;
@@ -32,4 +33,29 @@ function liner_search(array $a, $key)
 $arr = [1, 2 ,3 ,4 ,5];
 $r = liner_search($arr, 6);
 var_dump($r);
+*/
+
+
+function liner_search(array $a, int $key)
+{
+    $i = 0;
+    array_push($a, $key);
+    while(true){
+        if($i == count($a)-1){
+            return false;
+        }
+
+        if($a[$i] == $key){
+            return $i;
+        }
+        $i++;
+    }
+}
+$arr = [1, 2, 3, 4, 5];
+$r = liner_search($arr, 2);
+if($r == false){
+    echo '원소가 없습니다.';
+} else {
+    echo '원소는 '.$r.'에 있습니다.';
+}
 
